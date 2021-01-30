035dab1160afd37ed2cb84f6c5bc5127f45d57f3
powergames20 <77201074+powergames20@users.noreply.github.com>
2021-01-30 16:05:54 +0100

    initial commit

---
+++ commands/maj.js
@@ -0,0 +1,8 @@
+module.exports = {
+    run: message => message.channel.send("(Mise a jour) Du Token"),
+    name: 'maj',
+    help: {
+        description: 'pour voire les mise a jour du bot future du serveur minucraft',
+        syntax: 'maj'
+    }
+}
