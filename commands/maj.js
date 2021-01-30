6d874f34a0b64394e88537e7649afad37d3a9e29
powergames20 <77201074+powergames20@users.noreply.github.com>
2021-01-30 16:16:49 +0100

    Update maj.js

--- commands/maj.js
+++ commands/maj.js
@@ -1,5 +1,5 @@
 module.exports = {
-    run: message => message.channel.send("(Mise a jour) Du Token"),
+    run: message => message.channel.send("(Mise a jour) de l'hebergeur"),
     name: 'maj',
     help: {
         description: 'pour voire les mise a jour du bot future du serveur minucraft',
